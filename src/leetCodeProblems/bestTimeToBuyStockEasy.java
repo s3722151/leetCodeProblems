cfff334fe8ffba3bb4f227bab9eaddb7a36cca97
Zetazone <37312796+Zetazone@users.noreply.github.com>
2025-02-13 18:32:58 +1100

    Just going through the answers for the question bestTimeToBuyStock. Also looked at nodes.

--- src/leetCodeProblems/bestTimeToBuyStockEasy.java
+++ src/leetCodeProblems/bestTimeToBuyStockEasy.java
@@ -1,15 +1,41 @@
 package leetCodeProblems;
 
 //Question: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-public class bestTimeToBuyStockEasy {
 
-    public int maxProfit(int[] prices) {
-		return 0;
-        
+
+
+//Method 2: One Traversal Solution – O(n) Time and O(1) Space
+//Change class to "solution" in LeetCode
+import java.util.ArrayList;
+import java.util.List;
+class bestTimeToBuyStockEasy  {
+
+    static int maxProfit(int[] prices) {
+        int minSoFar = prices[0];
+        int res = 0;
+
+        // Update the minimum value seen so far
+          // if we see smaller
+        for (int i = 1; i < prices.length; i++) {
+            minSoFar = Math.min(minSoFar, prices[i]);
+           
+            // Update result if we get more profit                
+            res = Math.max(res, prices[i] - minSoFar);
+        }
+        return res;
     }
 
+    public static void main(String[] args) {
+        int[] prices = {7, 10, 1, 3, 6, 9, 2};
+        System.out.println(maxProfit(prices));
+    }
 }
 
+
+
+
+//Take a look: https://www.youtube.com/watch?v=Clp5c7HvLqs
+//https://www.youtube.com/watch?v=Sz9yH-RDAgo
 
 //============================================================
 //Sources
@@ -25,6 +51,14 @@ Topics
 - What is it: https://www.geeksforgeeks.org/dynamic-programming/
 - Video course: https://www.freecodecamp.org/news/learn-dynamic-programming-in-java/
 
+Relevant information 
+Nodes: https://www.codecademy.com/learn/getting-started-with-data-structures-java/modules/nodes-java/cheatsheet
+Max: https://www.w3schools.com/java/ref_math_max.asp
+
+Solutions
+https://www.geeksforgeeks.org/best-time-to-buy-and-sell-stock/
+- Note that if array is in order you cannot sell at maximum, based on first value
+https://www.youtube.com/watch?v=3EehPI_BkFk
 
 What I have learnt 
 Dynamic Programming: algorithmic technique used to optimize recursive solutions
