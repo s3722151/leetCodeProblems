05d1aa8168c3952bee2aa65efffe23f54b9d117c
Zetazone <37312796+Zetazone@users.noreply.github.com>
2025-02-21 20:43:18 +1100

    Looked at more specific definitions of binary trees. Completed the invert binart tree problem by using a solution.

--- src/leetCodeProblems/invertBinaryTree.java
+++ src/leetCodeProblems/invertBinaryTree.java
@@ -1,29 +1,51 @@
 package leetCodeProblems;
-//Question: 
+//Question:https://leetcode.com/problems/invert-binary-tree/ 
 public class invertBinaryTree {
 
 }
 
 //Solution 
-//class Solution {
-//    public TreeNode invertTree(TreeNode root) {
-//        
-//    }
+/*
+ class Solution {
+    public TreeNode invertTree(TreeNode root) {
+         // Base case: if the tree is empty, return null
+        if (root == null){
+        	return root; //This means there is nothing to invert
+        }
+        
+        // Recursively invert the left subtree
+        TreeNode left = invertTree(root.left);
+        
+        // Recursively invert the right subtree
+        TreeNode right = invertTree(root.right);
+        
+        // Swap the left and right children of the current node
+        root.right = left;
+        root.left = right;
+        
+        // Return the root of the inverted tree
+        return root;
+
+    }
+   }
+ */
+
 
 
 //============================================================
-//Solution explanations
+//Research topics
 //============================================================
 /*
 Topics
 1.Tree
 	https://www.geeksforgeeks.org/introduction-to-tree-data-structure/
+2.Nodes
 	https://www.codecademy.com/learn/getting-started-with-data-structures-java/modules/nodes-java/cheatsheet
-2. Depth-First Search(DFS)
+3. Depth-First Search(DFS)
 	https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
 	https://www.youtube.com/watch?v=Urx87-NMm6c
 	https://www.youtube.com/watch?v=by93qH4ACxo
-3. Breadth-First Search(BFS)
+4. Breadth-First Search(BFS)
 	https://www.youtube.com/watch?v=HZ5YTanv5QE
 	https://www.youtube.com/watch?v=7Cox-J7onXw
 
@@ -35,11 +57,14 @@ Questions to consider:
 	iii) DO we need to do a level order traversal?
 	iv) Do we need to find shortest path between 2 nodes
 
-4. Binary Tree
+5. Binary Tree
+	https://www.geeksforgeeks.org/introduction-to-binary-tree/
+	
+
 
 Solution 
-https://www.youtube.com/watch?v=fKgZiCXb6zs
-
+	https://www.youtube.com/watch?v=fKgZiCXb6zs
+	More detailed: https://www.youtube.com/watch?v=JUyhWTDT2YI
 
 Explanations
 Tree data structure is a hierarchical structure that is non-linear
@@ -65,5 +90,33 @@ BFS is an algorithm to search a graph
 - Difference is that we visit nodes at same level before we progress vertically
 - This means before we reach bottom/end of graph (leaf)
 
+Binary Tree
+This is a non-linar data structure
+Top most node is "root", bottom-most node is called "leaves"
+ */
 
+//============================================================
+//Solution explanations
+//============================================================
+/*
+(Base)Here is a binary tree
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+(Middle)First switch the child nodes
+     4
+   /   \
+  2     7
+ / \   / \
+3   1 9   6
+
+(Final)After inverting parent nodes:
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
  */
